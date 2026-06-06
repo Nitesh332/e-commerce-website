5827880bdf809fe47ddc48525daa68969470235a
Nitesh Sawant <niteshsawant@Niteshs-MacBook-Air.local>
2026-06-06 21:57:14 +0530

    add pages

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -45,6 +45,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../src/app/update/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/update">> = Specific
+  const handler = {} as typeof import("../../src/app/update/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
